f7e8c033699fee0ad6756eeba289119b2b9ac15a
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2025-02-01 23:34:51 +0530

    feat(86cxr5bu6): :closed_lock_with_key: login and authenticate v1

--- src/utilProviders/paseto.util.provider.ts
+++ src/utilProviders/paseto.util.provider.ts
@@ -3,9 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import loggernaut from 'loggernaut';
 import { V3 } from 'paseto';
 
-
 interface PasetoPayload {
-  [key: PropertyKey]: unknown;
+  [key: PropertyKey]: string;
 }
 
 @Injectable()
@@ -16,22 +15,32 @@ export class PasetoProvider {
     try {
       return await V3.generateKey('local', {
         format: 'paserk',
-      });    
+      });
     } catch (error) {
       loggernaut.error(error.message);
       throw new Error('Failed to generate PASETO local key.');
     }
   }
 
-  async generateEncryptedToken(payload: PasetoPayload, PASETO_LOCAL_KEY:string) {
+  async generateEncryptedToken(
+    payload: PasetoPayload,
+    PASETO_LOCAL_KEY: string,
+  ) {
     try {
-      console.log('>> ################################################## <<');
-      console.log(this.configService.get('PASETO.audience'))
-      console.log(this.configService.get('PASETO.issuer'))
       return await V3.encrypt(payload, PASETO_LOCAL_KEY, {
+        assertion: this.configService.get('PASETO.audience'),
         audience: this.configService.get('PASETO.audience'),
+        expiresIn: this.configService.get('PASETO.token_life'),
+        iat: true,
         issuer: this.configService.get('PASETO.issuer'),
-        expiresIn: '2 hours',
+        // jti: "unique-token-id", // Replace with a generated UUID
+        // kid: "key-id-1234", // Replace with a unique key identifier
+        notBefore: this.configService.get('PASETO.not_before'),
+        now: new Date(),
+        subject: payload.user_id,
+        footer: {
+          purpose: this.configService.get('PASETO.purpose'),
+        },
       });
     } catch (error) {
       loggernaut.error(error.message);
@@ -42,13 +51,15 @@ export class PasetoProvider {
   async decodeEncryptedToken(token: string, pasetoKey: string) {
     try {
       return await V3.decrypt(token, pasetoKey, {
-        audience: 'urn:example:client',
-        issuer: 'https://op.example.com',
-        clockTolerance: '1 min'
-      })
+        assertion: this.configService.get('PASETO.audience'),
+        audience: this.configService.get('PASETO.audience'),
+        issuer: this.configService.get('PASETO.issuer'),
+        maxTokenAge: this.configService.get('PASETO.max_token_age'),
+        clockTolerance: this.configService.get('PASETO.clock_tolerance'),
+      });
     } catch (error) {
       loggernaut.error(error.message);
-      throw new Error('Failed to decode the encrypted token.')
+      throw new Error('Failed to decode the encrypted token.');
     }
   }
 }
