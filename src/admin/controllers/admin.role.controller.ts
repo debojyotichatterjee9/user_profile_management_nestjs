271a0f32738e0b1b8cd5a96a209fb8f6622eaf6d
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2025-05-11 20:56:51 +0530

    feat(CU-86cyx5x5kuh): Add role management DTOs, services, and controller updates
    Introduce DTOs for role operations and implement methods in the RolesService to handle CRUD with pagination, soft delete, and data validation. Update the RolesController to integrate new features and enhance response handling.
    
    Signed-off-by: Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>

--- src/admin/controllers/admin.role.controller.ts
+++ src/admin/controllers/admin.role.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { RolesService } from '../services/admin.role.service';
 import { Role } from '../entities/role.entity';
 import { Serialize } from '../../interceptors/serialize.interceptor';
 import { CreateGenericResponseDto } from '../dto/response.dtos/create.generic.response.dto';
 import { CreateRoleDto } from '../dto/request.dtos/create.role.dto';
+import { RoleListResponseDto } from '../dto/response.dtos/role.list.dto.response';
+import { PaginationQueryParams } from '../dto/request.dtos/generic.pagination.list.dto';
+import { GenericCreateUpdateDeleteResponseDto } from '../dto/response.dtos/generic.create.update.delete.response.dto';
+import { UpdateRoleDto } from '../dto/request.dtos/update.role.dto';
 
 @Controller('admin/role')
 export class RolesController {
@@ -15,18 +28,29 @@ export class RolesController {
     return this.rolesService.create(role);
   }
 
+  @Serialize(RoleListResponseDto)
   @Get('list')
-  findAll(): Promise<Role[]> {
-    return this.rolesService.findAll();
+  findAll(@Query() queryParams: PaginationQueryParams): Promise<Role[]> {
+    return this.rolesService.findAll(queryParams);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string): Promise<Role> {
-    return this.rolesService.findOne(+id);
+    return this.rolesService.findOne(id);
   }
 
+  @Serialize(GenericCreateUpdateDeleteResponseDto)
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() payload: UpdateRoleDto,
+  ): Promise<Role> {
+    return this.rolesService.update(id, payload);
+  }
+
+  @Serialize(GenericCreateUpdateDeleteResponseDto)
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<void> {
-    return this.rolesService.remove(+id);
+  remove(@Param('id') id: string): Promise<Role> {
+    return this.rolesService.remove(id);
   }
 }
