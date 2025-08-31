d6f12b634cd4d5989d1a83f72f7a24374296d603
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2025-09-01 01:37:34 +0530

    feat(86d04m96e): change authentication to authorization in the headers name

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -34,8 +34,7 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     const request = context.switchToHttp().getRequest();
-    const [type, token] = request.headers.authentication?.split(' ') ?? [];
-
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
     if (type !== 'Bearer' || !token) {
       throw new UnauthorizedException('Missing authentication token');
     }
